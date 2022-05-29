2d7ad905e4f6f446afbe6e6a3a8bbfdb1648fcb7
david <david@LETO>
2022-05-28 19:40:24 -0600

    Add project files.

---
+++ ExpandedHungerConfig.cs
@@ -0,0 +1,25 @@
+﻿
+namespace ExpandedHunger
+{
+    internal class ExpandedHungerConfig
+    {
+        public float MaxSaturation;
+		public bool PukeOnDeath = true;
+		public float DrainOnDeath = 0f;
+		public float DeathCheckPrecision = 2f;
+
+		public ExpandedHungerConfig()
+		{
+
+		}
+
+		public void ResetToDefaults()
+		{
+			MaxSaturation = 3000f;
+			PukeOnDeath = true;
+			DrainOnDeath = 0f;
+			DeathCheckPrecision = 2f;
+		}
+	}
+
+}
